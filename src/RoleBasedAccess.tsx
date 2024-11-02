b1595d2d8712ca8fa545ee1db2f551479589ca3c
delower-hasan <dhhasansaha11@gmail.com>
2024-11-02 11:17:12 +0600

    project initialize

---
+++ src/RoleBasedAccess.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from "react";
+import { useUserRole, UserRoleType } from "./UserRoleContext";
+
+interface RoleBasedAccessProps<T extends UserRoleType> {
+  can?: T[]; // Only these roles can see the component
+  canNot?: T[]; // These roles cannot see the component
+  children: ReactNode; // The children to render if the role matches
+}
+
+const RoleBasedAccess = <T extends UserRoleType>({
+  can,
+  canNot,
+  children,
+}: RoleBasedAccessProps<T>) => {
+  const { role: userRole } = useUserRole<T>();
+
+  // Check for mutually exclusive props
+  if (can && canNot) {
+    throw new Error(
+      "RoleBasedAccess: Provide either 'can' or 'canNot', not both."
+    );
+  }
+
+  // Determine if the component should render based on the 'can' prop
+  if (can && !can.includes(userRole)) {
+    return null;
+  }
+
+  // Determine if the component should render based on the 'canNot' prop
+  if (canNot && canNot.includes(userRole)) {
+    return null;
+  }
+
+  // If none of the conditions are met, render the children
+  return <>{children}</>;
+};
+
+export default RoleBasedAccess;
---
+++ src/UserRoleContext.tsx
@@ -0,0 +1,50 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+// Define possible roles as strings, but allow flexibility for custom roles
+export type UserRoleType = string;
+
+interface UserRoleContextType<T extends UserRoleType> {
+  role: T;
+  setRole: Dispatch<SetStateAction<T>>;
+}
+
+interface UserRoleProviderProps<T extends UserRoleType> {
+  children: ReactNode;
+  initialRole: T;
+}
+
+// Create a context with a generic type parameter
+const UserRoleContext = createContext<UserRoleContextType<any> | undefined>(
+  undefined
+);
+
+export const UserRoleProvider = <T extends UserRoleType>({
+  children,
+  initialRole,
+}: UserRoleProviderProps<T>) => {
+  const [role, setRole] = useState<T>(initialRole);
+
+  return (
+    <UserRoleContext.Provider value={{ role, setRole }}>
+      {children}
+    </UserRoleContext.Provider>
+  );
+};
+
+// Custom hook to use the context with generic role type
+export const useUserRole = <
+  T extends UserRoleType
+>(): UserRoleContextType<T> => {
+  const context = useContext(UserRoleContext);
+  if (!context) {
+    throw new Error("useUserRole must be used within a UserRoleProvider");
+  }
+  return context as UserRoleContextType<T>;
+};
